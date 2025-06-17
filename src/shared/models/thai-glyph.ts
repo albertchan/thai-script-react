26a3b1936700445b003263f806ad706a9e8f9f5f
Albertc <atfchan@gmail.com>
2025-06-18 01:04:47 +0800

    fix: duplicate attribute

--- src/shared/models/thai-glyph.ts
+++ src/shared/models/thai-glyph.ts
@@ -6,7 +6,6 @@ export default interface ThaiGlyph {
   description: string;
   transcript: string;
   type?: string;
-  type: string;
   soundStart: string;
   soundEnd: string;
   toneClass: string;
