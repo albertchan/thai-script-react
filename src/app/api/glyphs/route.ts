731a65cd3a8d65f17eee683cba2c459e2c3773ce
Albertc <atfchan@gmail.com>
2025-06-18 13:11:13 +0800

    fix: json import

--- src/app/api/glyphs/route.ts
+++ src/app/api/glyphs/route.ts
@@ -1,7 +1,9 @@
-import glyphs from "../../../../public/thai-script.json";
-
 export async function GET() {
-  return Response.json(glyphs, {
+  const baseUrl = `${process.env.PROTOCOL_SCHEME}${process.env.VERCEL_URL}`;
+  const res = await fetch(`${baseUrl}/thai-script.json`);
+  const data = await res.json();
+
+  return Response.json(data, {
     status: 200,
     headers: { 'Content-Type': 'application/json' }
   });
--- src/app/api/vowels/route.ts
+++ src/app/api/vowels/route.ts
@@ -1,6 +1,8 @@
-import data from "../../../../public/thai-vowels.json";
-
 export async function GET() {
+  const baseUrl = `${process.env.PROTOCOL_SCHEME}${process.env.VERCEL_URL}`;
+  const res = await fetch(`${baseUrl}/thai-vowels.json`);
+  const data = await res.json();
+
   return Response.json(data, {
     status: 200,
     headers: { 'Content-Type': 'application/json' }
